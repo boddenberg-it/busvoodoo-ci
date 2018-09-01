b1aa7b44891df59f7fb5b25e4a5075dd38290cff
André Boddenberg <andre@boddenberg.it>
2018-09-01 13:51:55 +0200

    Fix docker run invokation + different wording in doc

--- build_slaves/build_slaves.sh
+++ build_slaves/build_slaves.sh
@@ -4,20 +4,20 @@
 # for build pipelines on https://jenkins.blobb.me.
 #
 # This script is also used to re-build latest
-# images on jenkins salves themselves.
+# images on jenkins slaves themselves weekly.
 #
 # Morover, one can use these images to build locally
 # with a different distro. This can be achieved by
-# following invokation after invoking this script:
+# following invokation after desired image is build:
 #
 # docker run -it --rm=true \
 #	-v "$BUSVOODOO_REPO":/build \
 #	"$BUSVOODOO_IMAGE" \
-#	BUSVOODOO_HARDWARE_VERSION="$HARDWARE_VERSION"
+#	BUSVOODOO_HARDWARE_VERSION="$HARDWARE_VERSION" rake
 #
 # HARDWARE_VERSION = {0,1}
-# BUSVOODOO_IMAGE = {archlinux_build, stretch_build,...}
-# BUSVOODOO_REPO = path to busvoodoo repo/branch
+# BUSVOODOO_IMAGE = {busvoodoo:archlinux_build,...}
+# BUSVOODOO_REPO = path to checked out busvoodoo repo/branch
 
 cd arch/
 docker build -t "busvoodoo:archlinux_build" .
