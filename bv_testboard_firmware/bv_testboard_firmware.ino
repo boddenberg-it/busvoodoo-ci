0e1a64b502c18a14cae6b36d067169a9558e7ac0
André Boddenberg <andre@boddenberg.it>
2018-09-07 23:12:08 +0200

    TB: Fix compiler errors/warnings

--- bv_testboard_firmware/bv_testboard_firmware.ino
+++ bv_testboard_firmware/bv_testboard_firmware.ino
@@ -1,55 +1,45 @@
-/*
- *
- */ 
-void setup() {
-
+  // PINS //
   // multiplexer (MP)
-  int 10 = MP_S0;
-  int 11 = MP_S1;
-  int 12 = MP_S2;
-  int 13 = MP_S3;
-  int 15 = MP_EN;
-  int 16 = MP_VCC;
+  int MP_S0 = 10;
+  int MP_S1 = 11;
+  int MP_S2 = 12;
+  int MP_S3 = 13;
+  int MP_EN = 14;
+  int MP_VCC = 15;
 
   // BusVoodoo (BV)
-  int 17 = BV_DFU_MODE;
-  // photo resistors to verify LED status
-  int 19 = BV_PR_1;
-  int 20 = BV_PR_2;
+  int BV_DFU_MODE = 16;
 
   // USB hub resets
-  int 18 = RESET_BUSVOODOO;
-  int 21 = RESET_FLASHBOARD;
-  int 22 = RESET_YOURSELF;
+  int RESET_BUSVOODOO = 17;
+  int RESET_FLASHBOARD = 18;
+  int RESET_YOURSELF = 19;
 
+void setup() {
+  // I/O modes
   pinMode(MP_S0, OUTPUT); 
   pinMode(MP_S1, OUTPUT); 
   pinMode(MP_S2, OUTPUT); 
   pinMode(MP_S3, OUTPUT); 
   pinMode(MP_EN, OUTPUT); 
   pinMode(MP_VCC, OUTPUT); 
-  
   pinMode(BV_DFU_MODE, OUTPUT); 
-  pinMode(BV_PR_1, INPUT); 
-  pinMode(BV_PR_2, INPUT); 
-  
   pinMode(RESET_BUSVOODOO, OUTPUT); 
   pinMode(RESET_FLASHBOARD, OUTPUT); 
   pinMode(RESET_YOURSELF, OUTPUT); 
 
+  // pulling every output low
   digitalWrite(MP_S0, LOW);
   digitalWrite(MP_S1, LOW);
   digitalWrite(MP_S2, LOW);
   digitalWrite(MP_S3, LOW);
   digitalWrite(MP_EN, LOW);
   digitalWrite(MP_VCC, LOW);
-
   digitalWrite(BV_DFU_MODE, LOW);
-
   digitalWrite(RESET_BUSVOODOO, LOW);
   digitalWrite(RESET_FLASHBOARD, LOW);
   digitalWrite(RESET_YOURSELF, LOW);
-  
+
   Serial.begin(9600);
   Serial.println("BusVoodoo testboard initialised...");
 }
@@ -59,30 +49,30 @@ void loop() {
   if (Serial.available() > 0) {
     char request = Serial.read();
     switch(request) {
-      case 'a':
+      case 'a': {
         reset_all();
-        break;
-      case 'b':
+      } break;
+      case 'b': {
         boot_bv_into_dfu_mode();
-        break;
-      case 'p':
+      } break;
+      case 'p': {
         ping();
-        break;
-      case 'r':
+      } break;
+      case 'r': {
         Serial.println('w');
         delay(1500);
         char input = Serial.read();
         reset(input);
-        break;
-      case 's':
+      } break;
+      case 's': {
         Serial.println('w');
         delay(1500);
-        String input = Serial.read();
+        int input = Serial.read();
         set_multiplexer(input);
-        break;
-      case 'd':
+      } break;
+      case 'd': {
         disable_multiplexer();
-        break;
+      } break;
       default:
         Serial.println("command not found");
     }
@@ -102,6 +92,13 @@ void reset_all(){
   // no ack can be send
 }
 
+char getState(int pin) {
+  if(digitalRead(pin)) {
+    return '1';
+  }
+  return '0';
+}
+
 void get_multiplexer() {
   String result ="";
   result = result + getState(MP_S0);
@@ -111,32 +108,25 @@ void get_multiplexer() {
   ack("get_multiplexer() -> " + result);
 }
 
-char getState(String pin) {
-  if(digitalRead(pin)) {
-    return '1';
-  }
-  return '0';
-}
-
 void ping() {
   ack("pong");
 }
 
 void reset(char device) {
   switch(device) {
-    case 'b':
+    case 'b': {
       digitalWrite(RESET_BUSVOODOO, HIGH);
       delay(500);
       digitalWrite(RESET_BUSVOODOO, LOW);
-      break;
-    case 'f':
+    }break;
+    case 'f': {
       digitalWrite(RESET_FLASHBOARD, HIGH);
       delay(500);
       digitalWrite(RESET_FLASHBOARD, LOW);
-      break;
-    case 'y':
+    }break;
+    case 'y': {
       digitalWrite(RESET_YOURSELF, HIGH);
-      break;
+    }break;
     default:
       error("reset device nout known (b|f|y)");
   }
@@ -152,28 +142,28 @@ void disable_multiplexer() {
   ack("disable_multiplexer()");
 }
 
-void set_multiplexer(String result) {
+void set_multiplexer(int result) {
   // disable multiplexer
   digitalWrite(MP_EN, LOW);
   delay(500);
 
   // set multiplexer
-  if(result[0] == '1') {
+  if(bitRead(result, 0) == 1) {
     digitalWrite(MP_S0, HIGH);
   } else {
     digitalWrite(MP_S0, LOW);
   }
-  if(result[1] == '1') {
+  if(bitRead(result, 1) == 1) {
     digitalWrite(MP_S1, HIGH);
   } else {
     digitalWrite(MP_S1, LOW);
   }
-  if(result[2] == '1') {
+  if(bitRead(result, 2) == 1) {
     digitalWrite(MP_S2, HIGH);
   } else {
     digitalWrite(MP_S2, LOW);
   }
-  if(result[3] == '1') {
+  if(bitRead(result, 3) == 1) {
     digitalWrite(MP_S3, HIGH);
   } else {
     digitalWrite(MP_S3, LOW);
