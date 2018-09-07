2fef6bd3af5c8b3fe8d82755ce654a320ee8d087
André Boddenberg <andre@boddenberg.it>
2018-09-07 21:09:55 +0200

    TB: Setup pins and init serial cnx

--- bv_testboard_firmware/bv_testboard_firmware.ino
+++ bv_testboard_firmware/bv_testboard_firmware.ino
@@ -8,31 +8,54 @@ void setup() {
   int 11 = MP_S1;
   int 12 = MP_S2;
   int 13 = MP_S3;
-  int 14 = MP_S4;
   int 15 = MP_EN;
   int 16 = MP_VCC;
 
   // BusVoodoo (BV)
   int 17 = BV_DFU_MODE;
-  int 18 = BV_RESET;
   // photo resistors to verify LED status
   int 19 = BV_PR_1;
   int 20 = BV_PR_2;
-  
-  digitalWrite(S2, LOW);
-  digitalWrite(S3, LOW);
-  digitalWrite(S4, LOW);
-  digitalWrite(EN, LOW);
-  ...
-  
-  // power cycle multiplexer (MP)
-  digitalWrite(VCC, HIGH);
 
+  // USB hub resets
+  int 18 = RESET_BUSVOODOO;
+  int 21 = RESET_FLASHBOARD;
+  int 22 = RESET_YOURSELF;
+
+  pinMode(MP_S0, OUTPUT); 
+  pinMode(MP_S1, OUTPUT); 
+  pinMode(MP_S2, OUTPUT); 
+  pinMode(MP_S3, OUTPUT); 
+  pinMode(MP_EN, OUTPUT); 
+  pinMode(MP_VCC, OUTPUT); 
+  
+  pinMode(BV_DFU_MODE, OUTPUT); 
+  pinMode(BV_PR_1, INPUT); 
+  pinMode(BV_PR_2, INPUT); 
+  
+  pinMode(RESET_BUSVOODOO, OUTPUT); 
+  pinMode(RESET_FLASHBOARD, OUTPUT); 
+  pinMode(RESET_YOURSELF, OUTPUT); 
+
+  digitalWrite(MP_S0, LOW);
+  digitalWrite(MP_S1, LOW);
+  digitalWrite(MP_S2, LOW);
+  digitalWrite(MP_S3, LOW);
+  digitalWrite(MP_EN, LOW);
+  digitalWrite(MP_VCC, LOW);
+
+  digitalWrite(BV_DFU_MODE, LOW);
+
+  digitalWrite(RESET_BUSVOODOO, LOW);
+  digitalWrite(RESET_FLASHBOARD, LOW);
+  digitalWrite(RESET_YOURSELF, LOW);
+  
   Serial.begin(9600);
 }
 
 void loop() {
-  
+
+        // 
         if (Serial.available() > 0) {
                 // read the incoming byte:
                 incomingByte = Serial.read();
